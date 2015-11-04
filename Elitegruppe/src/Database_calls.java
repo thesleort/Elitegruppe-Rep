10ad5c9b77f3baa21a8f0579cc50756551f2a047
mark jervelund <mark@jervelund.com>
2015-11-04 13:58:48 +0100

    Calls for database where information is stored

---
+++ Elitegruppe/src/Database_calls.java
@@ -0,0 +1,14 @@
+
+public class Database_calls {
+	self.CPR#
+	self.CPR.FirstName =
+	self.CPR.MiddleName =
+	self.CPR.LastName =
+	self.CPR.DateOfBirth =
+	self.CPR.DateOfDeath =
+	self.CPR.Mother =
+	self.CPR.Father =
+	self.CPR.Childeren =
+	
+	
+}
