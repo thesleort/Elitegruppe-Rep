445e824d9cef65dca2e03dd5f0c7236d1929e711
Osman <Osman@Osman>
2015-11-04 14:00:06 +0100

    Jeg har lavet en println hilsen

--- Elitegruppe/src/HelloWorld.java
+++ Elitegruppe/src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 		System.out.println("Hello, World!");
 		System.out.println("HEllo you!");
 		System.out.println("HEllo Everyone!");
+		System.out.println("hej hilsen Osman");
 	}
 }
