3f2854c7d053ada9a773a03d249fba5e4560d1a5
mark jervelund <mark@jervelund.com>
2015-11-11 13:04:33 +0100

    Updated some comment for TeamGUI.java , and added names to the project

--- Elitegruppe/src/TeamGUI.java
+++ Elitegruppe/src/TeamGUI.java
@@ -1,12 +1,17 @@
-
-
+//import libs needed
 import java.io.*;
 
+//defines the class
 public class TeamGUI{
+	//defines the void
     public static void main(String[] args){
+    	//try (says what it does itself
         try{
+        	//opens the file with the names in it
             File file = new File("names.txt");
+            //prints the path to the file
             System.out.println(file.getCanonicalPath());
+            //loads the file
             FileInputStream ft = new FileInputStream(file);
 
             DataInputStream in = new DataInputStream(ft);
@@ -14,7 +19,12 @@ public class TeamGUI{
             String strline;
 
             while((strline = br.readLine()) != null){
-                System.out.println(strline);
+               	String[] parts = strline.split(" ");
+               	String[] partname = strline.split("");
+               	String firstname = partname[0];
+               	String lastname = parts[1];
+                System.out.println(firstname+". "+lastname);	
+            //    System.out.println(strline);
             }
             in.close();
         }catch(Exception e){
