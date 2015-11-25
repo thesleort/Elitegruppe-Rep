2084dc6884e908d03ef499491d893bc9abf511ee
GIT Admin <troelsblicherpetersen@gmail.com>
2015-11-25 16:57:52 +0100

    Ny program til håntering af filer.
    Den nye "FileOperation.java" er en forbedret udgave af "OpenFile.java".
    Blandt de nye funktionaliteter findes:
    - Modulært design.
    --- Man kan nu nøjes med at læse filen ind i en ArrayList og undlade at
    skrive til en nye fil.
    --- Man kan efterfølgende skrive til en ny file ved at kalde .FileWrite
    på klassen.

--- Elitegruppe/src/FamilyTree.java
+++ Elitegruppe/src/FamilyTree.java
@@ -1,11 +1,23 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
 import ui.Gui;
 
 public class FamilyTree {
 
 	public static void main(String[] args) {
 		System.out.println("Starting program...");
-		Gui gui = new Gui();
+		//Gui gui = new Gui(); Denne er fjernet, da det ikke kører noget brugbart ligenu.
 		System.out.println("GUI has started.");
-		OpenFile openfile = new OpenFile();
+		FileOperation fileop = new FileOperation();
+		try {
+			ArrayList<String> names = fileop.FileRead("names.txt");
+			System.out.println("done");
+			fileop.FileWrite(names);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
---
+++ Elitegruppe/src/FileOperation.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class FileOperation {
+	public ArrayList FileRead(String fileName) throws IOException {
+		File file = new File(fileName);
+		FileInputStream FiStream;
+		ArrayList<String> namesToSort = new ArrayList<String>();
+		try {
+			FileInputStream fiStream = new FileInputStream(file);
+			DataInputStream diStream = new DataInputStream(fiStream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(diStream));
+
+			String currentLine;
+			
+			while ((currentLine = br.readLine()) != null) {
+				while ((currentLine = br.readLine()) != null) {
+					// spilts each line into a first and last name.
+					String[] parts = currentLine.split(" ");
+					// spilts the first name into letters
+					String[] partname = currentLine.split("");
+					// stores the first letter of the name in "firstname"
+					String firstname = partname[0];
+					// stores the last name in the string "lastname"
+					String lastname = parts[1];
+					// combines the string and prints it.
+					System.out.println(firstname + ". " + lastname);
+					// debudding line
+					// System.out.println(strline);
+				}
+					System.out.println(currentLine);
+					namesToSort.add(currentLine);
+
+			}
+			diStream.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("There where no file with the name " + fileName);
+			e.printStackTrace();
+		} 
+		return(namesToSort);
+	}
+	public void FileWrite(ArrayList<String> arrayOfNames) throws FileNotFoundException, UnsupportedEncodingException {
+		Collections.sort(arrayOfNames);
+		PrintWriter writer = new PrintWriter("SortedNames", "UTF-8");
+		writer.println(arrayOfNames);
+		writer.close();
+	}
+}
